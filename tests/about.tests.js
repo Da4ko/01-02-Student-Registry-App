ac41d29ab6ee89dd254c99e93a597143d30042b4
Da4ko <da4ko06@gmail.com>
2024-12-29 14:35:57 +0200

    new change

--- tests/about.tests.js
+++ tests/about.tests.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const fetch = require('node-fetch');
-//const comment s
+//const comment s add new update comment
 suite('About page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8888/about");
